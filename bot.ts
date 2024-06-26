e48cf600bdadea9c39380f6e0292b6d77b7b82be
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-06-26 22:06:49 +0300

    Error handling & command menu

--- bot.ts
+++ bot.ts
@@ -1,5 +1,9 @@
 import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-import { Bot } from "https://deno.land/x/grammy@v1.25.1/mod.ts";
+import {
+  Bot,
+  GrammyError,
+  HttpError,
+} from "https://deno.land/x/grammy@v1.25.1/mod.ts";
 
 const env = await load();
 const botKey = env["BOT_API_KEY"];
@@ -8,13 +12,40 @@ const bot: Bot = new Bot(botKey);
 
 bot.api.setMyCommands([
   {
+    command: "name",
+    description: "Цытата паводле імені філосафа",
+  },
+  {
+    command: "topic",
+    description: "Цытата паводле тэмы",
+  },
+  {
     command: "help",
     description: "Як працуе бот?",
+  },
+  {
+    command: "works",
+    description: "Працы, выкарыстаныя пры стварэнні бота",
   },
 ]);
 
 bot.command("start", (ctx) => {
-  ctx.reply("Hello, philosopher!");
+  ctx.reply("Вітанкі! Я <b>бот-філосаф</b>!", {
+    parse_mode: "HTML",
+  });
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
 });
 
 bot.start();
